af01da8a703f90b23395ddd67e2bb9b7960462d3
bxjz <76890339+bxjz@users.noreply.github.com>
2023-06-10 16:21:54 -0400

    Update genre_dropdown.py

--- genre_dropdown.py
+++ genre_dropdown.py
@@ -1,26 +1,33 @@
 from dash import Dash, html, dcc
 from .ids import *
 import dash_bootstrap_components as dbc
-from . import (pieChart, genre_dropdown)
+from dash.dependencies import Input, Output
 
-def create_layout(app,data):
-    heading = html.H1(
-        'Game Sales',
-        className = 'bg-primary text-secondary p-2 mb-3'
+
+def render(app,data):
+    all_genres = data['genre'].unique()
+
+    @app.callback(
+        Output(GENRE_DROPDOWN, "value"),
+        Input(SELECT_ALL_GENRES, "n_clicks")
     )
-    return dbc.Container([
-        dbc.Row([
-            dbc.Col(heading)
-        ]),
-        dbc.Row([
-            dbc.Col(
-                html.Div(
-                [
-                    genre_dropdown.render(app,data), 
-                   
-                ],
-                className="dropdown-container"
-                ),lg= 6),
-            dbc.Col(pieChart.render(app,data), lg =6),
-        ]),
-     ])
+    def select_all_genres(n):
+        return all_genres
+
+    dropdown = html.Div(
+        [
+            html.H6("Genre"),
+            dcc.Dropdown(
+                id=GENRE_DROPDOWN,
+                options=[{"label":genre, "value":genre} for genre in all_genres],
+                multi=True,
+            ),
+            html.Button(
+                id=SELECT_ALL_GENRES,
+                children=["Select All"],
+                className="dropdown-button",
+                n_clicks=0
+                ),
+        ]
+    )
+    return dropdown
